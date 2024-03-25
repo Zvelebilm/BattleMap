1bdfdc1eab0d40d0908b0763f941cfc48498fdd3
Marek Zvelebil <zvelebil.marek@gmail.com>
2024-03-25 15:49:49 +0100

    init commit

---
+++ BattleMap/src/main/java/com/example/battlemap/BattleMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.battlemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleMapApplication.class, args);
+	}
+
+}
---
+++ BattleMap/src/test/java/com/example/battlemap/BattleMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.battlemap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
