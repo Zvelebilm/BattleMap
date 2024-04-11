6f500f992e5d36a4e35d051d24b363dc4927d02a
Marek Zvelebil <zvelebil.marek@gmail.com>
2024-04-11 15:57:28 +0200

    logging when saving game, typo in FE, popup on click showing name, button swap

--- BattleMap/src/main/java/com/example/battlemap/model/RepositoryService.java
+++ BattleMap/src/main/java/com/example/battlemap/model/RepositoryService.java
@@ -1,8 +1,9 @@
 package com.example.battlemap.model;
 
 import com.example.battlemap.repository.GameRepo;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
-
+@Log4j2
 @Service
 public class RepositoryService {
     GameRepo gameRepo;
@@ -13,5 +14,7 @@ public class RepositoryService {
 
     public void saveGame(Game game) {
         gameRepo.save(game);
+        log.info("game saved: " + game.getGameName());
+        //todo only one game can be created because Name is @Id and is all-times same...
     }
 }
